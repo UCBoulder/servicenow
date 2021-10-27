a67e1e46c5cd4d079e3e1e61bceb91287b36bca7
France <miles.france@gmail.com>
2021-10-27 19:57:30 +0000

    starting to add limits and offset

--- src/Plugin/PrincessList.php
+++ src/Plugin/PrincessList.php
@@ -11,7 +11,7 @@ class PrincessList {
   /**
    * Establish Database connection.
    *
-   * @var string
+   * @var object
    */
   private $princessDbConnection;
 
@@ -32,7 +32,7 @@ class PrincessList {
   /**
    * Princess list settings.
    *
-   * @var string
+   * @var object
    */
   private $princessSettings;
 
@@ -65,6 +65,13 @@ class PrincessList {
   private $plPenultimate;
 
   /**
+   * Princess set offset.
+   *
+   * @var int
+   */
+  private $plOffset;
+
+  /**
    * Pull Princess List (DDS) from cache or servicenow if not cached.
    */
   public function __construct() {
@@ -77,6 +84,11 @@ class PrincessList {
     $this->princessLastKey = array_key_last($pl_data);
     $this->princessSettings = new ServicenowFetchSettings();
     $this->princessReload = $this->princessSettings->getpr();
+
+    $this->plOffset = $this->princessDbConnection->select('princess_list', 'pl')
+      ->condition('id', $this->princessLastKey)
+      ->fields('pl', ['offset'])
+      ->execute()->fetch();
     // Use Second to last entry when rebuilding.
     $this->plPenultimate = $pl_data[count($pl_data) - 1];
     // Use Last entry when already built.
@@ -85,6 +97,9 @@ class PrincessList {
     $this->princessData = $this->princessReload ? $this->plPenultimate : $this->plEnd;
   }
 
+  /**
+   * Cron function to clean and insert into table.
+   */
   public function cron() {
     // Tidy up and clean out entries above 2.
     if ($this->princessFirstKey) {
@@ -92,50 +107,57 @@ class PrincessList {
         ->condition('id', $this->princessFirstKey)
         ->execute();
     }
-    if ($this->princessReload == 33) {
+    if ($this->princessReload) {
+      $pl_data = json_decode($this->plEnd);
       $api_call = new ServicenowApiCall();
       $dds_service_member_query = 'u_dds_service_request_group_member';
+      $set_limit = 10;
+      $query_limit = ['sysparm_limit' => $set_limit, 'sysparm_offset' => $this->plOffset];
       // $dds_service_member_query .= '?sysparm_limit=1';
-      $dds_service_members = $api_call->apiCallMeMaybe(0, 0, $dds_service_member_query, FALSE);
+      $dds_service_members = $api_call->apiCallMeMaybe($dds_service_member_query, $query_limit, 0, FALSE);
+      //$dds_service_members = $api_call->apiCallMeMaybe(0, 0, $dds_service_member_query, FALSE);
       $dds_service_group_query = 'u_dds_service_request_group';
-      // $dds_service_group_query .= '?sysparm_limit=80';
-      $dds_service_group = $api_call->apiCallMeMaybe(0, 0, $dds_service_group_query);
-      $service_members = [];
-      foreach ($dds_service_members->result as $service_member) {
-        if (($service_member->u_active == 'true') && isset($service_member->u_primary_dds_group->value) && isset($service_member->u_assignment_group->value)) {
-          $user_key = Xss::filter($service_member->u_user->value);
-          $service_members[$user_key]['sys_id'] = Xss::filter($user_key);
-          $service_members[$user_key]['user_name'] = Xss::filter($service_member->u_full_name);
-          $service_members[$user_key]['email'] = Xss::filter($service_member->u_user_email);
-          $service_members[$user_key]['dds_group'] = Xss::filter($service_member->u_primary_dds_group->value);
-          $service_members[$user_key]['assignment_group'] = Xss::filter($service_member->u_assignment_group->value);
-          $service_members[$user_key]['request_group'][] = Xss::filter($service_member->u_service_request_group->value);
-          if ($service_member->u_service_request_group->value == 'ad78a2cd1ba48c10566a43b3cd4bcb33') {
-            $sc_user = user_load_by_name(Xss::filter($service_member->u_user_name));
-            if (!empty($sc_user)) {
-              $sc_user->set('field_dds', 1);
-              $sc_user->save();
+      $dds_service_group = $api_call->apiCallMeMaybe($dds_service_group_query, $query_limit, 0);
+      if (!empty($dds_service_members)) {
+        $service_members = [];
+        foreach ($dds_service_members->result as $service_member) {
+          if (($service_member->u_active == 'true') && isset($service_member->u_primary_dds_group->value) && isset($service_member->u_assignment_group->value)) {
+            $user_key = Xss::filter($service_member->u_user->value);
+            $service_members[$user_key]['sys_id'] = Xss::filter($user_key);
+            $service_members[$user_key]['user_name'] = Xss::filter($service_member->u_full_name);
+            $service_members[$user_key]['email'] = Xss::filter($service_member->u_user_email);
+            $service_members[$user_key]['dds_group'] = Xss::filter($service_member->u_primary_dds_group->value);
+            $service_members[$user_key]['assignment_group'] = Xss::filter($service_member->u_assignment_group->value);
+            $service_members[$user_key]['request_group'][] = Xss::filter($service_member->u_service_request_group->value);
+            if ($service_member->u_service_request_group->value == 'ad78a2cd1ba48c10566a43b3cd4bcb33') {
+              $sc_user = user_load_by_name(Xss::filter($service_member->u_user_name));
+              if (!empty($sc_user)) {
+                $sc_user->set('field_dds', 1);
+                $sc_user->save();
+              }
             }
           }
         }
+        $pl_data['users'] = $service_members;
       }
       // Populate the groups.
-      $dept_list = [];
-      foreach ($dds_service_group->result as $dept) {
-        if ($dept->u_active == 'true') {
-          $dept_name = Xss::filter($dept->u_name);
-          $dept_code = Xss::filter($dept->sys_id);
-          $dept_list[$dept_code] = $dept_name;
+      if (!empty($dds_service_group)) {
+        $dept_list = [];
+        foreach ($dds_service_group->result as $dept) {
+          if ($dept->u_active == 'true') {
+            $dept_name = Xss::filter($dept->u_name);
+            $dept_code = Xss::filter($dept->sys_id);
+            $dept_list[$dept_code] = $dept_name;
+          }
         }
+        $pl_data['departments'] = $dept_list;
+        asort($pl_data['departments']);
       }
-      asort($dept_list);
-      $princess_list = [
-        'departments' => $dept_list,
-        'users' => $service_members,
-      ];
-      // $this->cachedData = $princess_list;
+      $new_offset = $set_limit + $this->plOffset;
+      kint($new_offset);
+      die();
       $princess_list = json_encode($princess_list);
-      $row = ['data' => $princess_list];
+      $row = ['data' => $princess_list, 'offset' => $new_offset];
       $this->princessDbConnection->update('princess_list')
         ->condition('id', $this->princessLastKey)
         ->fields($row)
@@ -155,10 +177,14 @@ class PrincessList {
    */
   public function reload() {
     $this->princessSettings->setpr(1);
-    $row = ['data' => ''];
+    $princess_list = [
+      'departments' => [],
+      'users' => [],
+    ];
+    $princess_list = json_encode($princess_list);
+    $row = ['data' => $princess_list];
     $db_connection = \Drupal::database();
     $db_connection->insert('princess_list')->fields($row)->execute();
   }
 
 }
-
