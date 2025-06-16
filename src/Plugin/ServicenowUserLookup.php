f817af4ecca7d20b801534ddf32d702ddf0470e1
protitude <miles.france@gmail.com>
2025-06-16 10:35:31 -0600

    Make sure email from servicenow is not null

--- src/Plugin/ServicenowUserLookup.php
+++ src/Plugin/ServicenowUserLookup.php
@@ -84,6 +84,9 @@ class ServicenowUserLookup {
     if (empty($account->getEmail())) {
       $query = ['user_name' => $account->getAccountName()];
       $result = $api_call->apiCallMeMaybe('sys_user', $query);
+      if ($result->result[0]->email == NULL) {
+        return;
+      }
       $account->setEmail(Xss::filter($result->result[0]->email));
       $account->save();
     }
